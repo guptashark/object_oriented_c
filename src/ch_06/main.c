cf2fd816a2853114bf0b7836947af28fbb48555d
Aishwary Gupta <guptashark@protonmail.com>
2023-01-01 01:03:28 -0500

    Compiling and not crashing.

--- src/ch_06/main.c
+++ src/ch_06/main.c
@@ -18,7 +18,7 @@ struct object {
 // The vtable.
 struct ag_std_vtable {
   // struct object obj; // We need to fix this.
-  char name[32];
+  // char name[32];
   struct ag_std_vtable *super;
   size_t size;
   void *(*ctor)(void *, va_list *);
@@ -45,13 +45,15 @@ void object_dtor(void *obj) {
 
 void object_print(void *obj) {
   struct ag_std_vtable *vt = *(struct ag_std_vtable **)obj;
-  printf("[object][print][%s]\n", vt->name);
+  (void)vt;
+  // printf("[object][print][%s]\n", vt->name);
+  printf("[object][print][]\n");
 }
 
 // The vtable of an object.
 struct ag_std_vtable object_vt = {
   // object struct.
-  "object", // name
+  // "object", // name
   NULL, // ptr to vtable of the super
   sizeof(struct object), // How come this doesn't work?
   object_ctor,
@@ -60,17 +62,43 @@ struct ag_std_vtable object_vt = {
 };
 
 // The ptr to a vtable of an object.
-struct ag_std_vtable *object = &object_vt;
+const struct ag_std_vtable *object = &object_vt;
 // const struct ag_std_vtable *object = &object_vt;
 
 // An instance of object... do we need this?
 
 // Vtable functions, which we may not need right now.
 void *vtable_ctor(void *obj, va_list *app) {
-  // TODO: Implement this.
-  (void)obj;
-  (void)app;
-  return NULL;
+  printf("[vtable][ctor]\n");
+
+  struct ag_std_vtable *self = (struct ag_std_vtable *)obj;
+  printf("[vtable][ctor]\n");
+
+  self->super = va_arg(*app, struct ag_std_vtable *);
+  printf("[vtable][ctor]\n");
+  self->size = va_arg(*app, size_t);
+  printf("[vtable][ctor]\n");
+
+  const size_t offset = offsetof(struct ag_std_vtable, ctor);
+  printf("[vtable][ctor][before_memcpy]\n");
+
+  printf("%zu\n", offset);
+  printf("%zu\n", self->super->size);
+
+  // TODO
+  // Need to implement the proper memcpy here...
+  /*
+  memcpy(
+      (char *)self + offset,
+      (char *)self->super + offset,
+      self->super->size - offset);
+  */
+
+  memcpy(self, self->super, sizeof(struct ag_std_vtable));
+
+  printf("[vtable][ctor]\n");
+
+  return obj;
 }
 
 void vtable_dtor(void *obj) {
@@ -85,17 +113,19 @@ void vtable_print(void *obj) {
 
 // An instance of vtable.
 // Looks like we can't create this just yet... and need to do it later.
+
 /*
 struct ag_std_vtable vtable = {
   // parent_obj, // ??
-  "vtable",
-  object, // vtable of the parent.
+  // "vtable",
+  (struct ag_std_vtable *)object, // vtable of the parent.
   sizeof(struct ag_std_vtable),
   vtable_ctor,
   vtable_dtor,
   vtable_print
 };
 */
+
 
 // Memory functions - new and delete.
 void *ag_std_new(const struct ag_std_vtable *vt, ...) {
@@ -133,6 +163,17 @@ void ag_std_print(void *obj) {
   struct ag_std_vtable *vt = *(struct ag_std_vtable **)obj;
   vt->print(obj);
 }
+
+/*
+size_t ag_std_sizeof(void *obj) {
+  struct ag_std_vtable *vt = *(struct ag_std_vtable **)obj;
+  return vt->size;
+}
+
+size_t ag_std_sizeof(struct ag_std_vtable *vt) {
+  return vt->size;
+}
+*/
 
 #if 0
 
@@ -214,7 +255,43 @@ const struct ag_std_vtable *derived_02 = &derived_02_vt;
 
 #endif
 
+// Integer inherits from object
+struct integer {
+  struct object obj;
+  int x;
+};
+
+void *integer_ctor(void *obj, va_list *app) {
+  (void)app;
+  printf("[integer][ctor]\n");
+  return obj;
+}
+
+void integer_dtor(void *obj) {
+  (void)obj;
+  printf("[integer][dtor]\n");
+}
+
+void integer_print(void *obj) {
+  (void)obj;
+  printf("[integer][print]\n");
+}
+
+// We need to create the vtable for integer using new.
+
 int main(void) {
+
+  struct ag_std_vtable vtable_vt = {
+    // parent_obj, // ??
+    // "vtable",
+    (struct ag_std_vtable *)object, // vtable of the parent.
+    sizeof(struct ag_std_vtable),
+    vtable_ctor,
+    vtable_dtor,
+    vtable_print
+  };
+
+  const struct ag_std_vtable *vtable = &vtable_vt;
 
   /*
   void *derived_01_obj = ag_std_new(derived_01, 1, 2);
@@ -228,6 +305,21 @@ int main(void) {
 
   void *obj = ag_std_new(object);
   ag_std_print(obj);
+
+  printf("creating a new class: integer\n");
+  void *integer = ag_std_new(
+      vtable,     // The type of object we are creating.
+      // "integer",  // The name of the object. (integer time)
+      object,     // The superclass.
+      sizeof(struct integer),       // The size of the integer structure.
+      ag_std_print, integer_print,  // The method of obj that we override.
+      0);
+
+  printf("creating a new instance of class: integer\n");
+  void *i = ag_std_new(integer, 4);
+  
+  ag_std_print(i);
+
   ag_std_delete(obj);
 
   return 0;
